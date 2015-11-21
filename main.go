83a8127a897db3d2cbcf221e6eb20d020e72aed6
himanshuo <ho2es@virginia.edu>
2015-11-21 14:57:03 -0500

    start websocket code

--- main.go
+++ main.go
@@ -5,8 +5,17 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"html/template"
+	"github.com/gorilla/websocket"
 
 )
+
+//The Conn type represents a WebSocket connection. 
+//websocket.Upgrader object uses upgrade function for a given http request handler 
+//to get a pointer to a conn.
+var upgrader = websocket.Upgrader{
+    ReadBufferSize:  1024,
+    WriteBufferSize: 1024,
+}
 
 func BaseHandler(w http.ResponseWriter, r *http.Request){
 	task := models.Task{Id:1, Name: "Himanshu Ojha"}
