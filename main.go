607cb31b3c78a3e970742b49d0db56db1c84b606
himanshuo <ho2es@virginia.edu>
2015-11-21 19:23:13 -0500

    save

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 )
 
 //The Conn type represents a WebSocket connection. 
+	//todo: is this correct?
+	//Conn is an interface and websocket implements it ???
 //websocket.Upgrader object uses upgrade function for a given http request handler 
 //to get a pointer to a conn.
 var upgrader = websocket.Upgrader{
@@ -18,6 +20,23 @@ var upgrader = websocket.Upgrader{
 }
 
 func BaseHandler(w http.ResponseWriter, r *http.Request){
+	
+	conn, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Println(err)
+        return
+    }
+
+    for {
+	    messageType, p, err := conn.ReadMessage()
+	    if err != nil {
+	        return
+	    }
+	    if err = conn.WriteMessage(messageType, p); err != nil {
+	        return err
+	    }
+	}
+
 	task := models.Task{Id:1, Name: "Himanshu Ojha"}
     t, _ := template.ParseFiles("templates/base.html")
     t.Execute(w, task)
