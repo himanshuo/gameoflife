1fbb6f152614fdf5728f584cf92d8ea90234b0cc
himanshuo <ho2es@virginia.edu>
2015-11-21 22:39:06 -0500

    create task WIP

--- main.go
+++ main.go
@@ -4,23 +4,58 @@ import (
 	"net/http"
 	"github.com/gorilla/mux"
 	"log"
+	"encoding/json"
 	"html/template"
 
 
 )
+var Tasks []models.Task
 
-func BaseHandler(w http.ResponseWriter, r *http.Request){
+//views
+func Home(w http.ResponseWriter, r *http.Request){
 	
+	t, _ := template.ParseFiles("templates/base.html")
+    t.Execute(w, Tasks)
 	
-	task := models.Task{Id:1, Name: "Himanshu Ojha"}
-    t, _ := template.ParseFiles("templates/base.html")
-    t.Execute(w, task)
 
 }
 
+
+//API
+func CreateTask(w http.ResponseWriter, r *http.Request){
+	taskName := r.PostFormValue("name")
+	newTask := models.Task{Id: len(Tasks), Name: taskName}
+
+
+    if err := json.NewEncoder(w).Encode(newTask); err != nil {
+        panic(err)
+    }
+}
+
+
 func main(){
 	r := mux.NewRouter()
-    r.HandleFunc("/", BaseHandler)
+
+	//views
+    r.HandleFunc("/", Home)
+    
+
+    //API
+
+    //task
+
+    //todo: can name each route in order to reverse them. 
+    s := r.PathPrefix("/task").Subrouter()
+    s.HandleFunc("/", CreateTask).Methods("PUT")
+    // s.HandleFunc("/{id:[0-9]+}/", UpdateTask).Methods("POST")
+    // s.HandleFunc("/", ViewAllTasks).Methods("GET")
+    // s.HandleFunc("/{id:[0-9]+}/", ViewTask).Methods("GET")
+    // s.HandleFunc("/{id:[0-9]+}/", DeleteTask).Methods("DELETE")
+    
+
+   
+    
+
     http.Handle("/", r)
     
 	log.Fatal(http.ListenAndServe(":8080", nil))
--- models/task.go
+++ models/task.go
@@ -1,6 +1,6 @@
 package models
 
 type Task struct {
-  Id int
-  Name string
+  Id int	`json:"id"`
+  Name string `json:"name"`
 }
