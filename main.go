b3ad2c73b6022f23a35a1a4ba56a97c6c9bad95d
himanshuo <ho2es@virginia.edu>
2015-11-21 19:23:40 -0500

    undo websocket stuff

--- main.go
+++ main.go
@@ -5,38 +5,13 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"html/template"
-	"github.com/gorilla/websocket"
+
 
 )
 
-//The Conn type represents a WebSocket connection. 
-	//todo: is this correct?
-	//Conn is an interface and websocket implements it ???
-//websocket.Upgrader object uses upgrade function for a given http request handler 
-//to get a pointer to a conn.
-var upgrader = websocket.Upgrader{
-    ReadBufferSize:  1024,
-    WriteBufferSize: 1024,
-}
-
 func BaseHandler(w http.ResponseWriter, r *http.Request){
 	
-	conn, err := upgrader.Upgrade(w, r, nil)
-    if err != nil {
-        log.Println(err)
-        return
-    }
-
-    for {
-	    messageType, p, err := conn.ReadMessage()
-	    if err != nil {
-	        return
-	    }
-	    if err = conn.WriteMessage(messageType, p); err != nil {
-	        return err
-	    }
-	}
-
+	
 	task := models.Task{Id:1, Name: "Himanshu Ojha"}
     t, _ := template.ParseFiles("templates/base.html")
     t.Execute(w, task)
