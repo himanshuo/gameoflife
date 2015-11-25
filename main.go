d0fc0f11118a64596911d84ff7525365ffda05ac
himanshuo <ho2es@virginia.edu>
2015-11-25 16:38:46 -0500

    update

--- main.go
+++ main.go
@@ -107,6 +107,40 @@ func CreateTask(w http.ResponseWriter, r *http.Request){
     }
 }
 
+
+func UpdateTask(w http.ResponseWriter, r *http.Request){
+	//note: r.FormValue searches for key in POST data fields, then PUT data fields
+	//2 types of POST submissions: application/x-www-form-urlencoded AND multipart/form-data.
+	// need to understand both. Generally speaking, urlencoded takes up extra space so is for normal post requests. multipart form-data does not increase space usage by a lot so is for uploading files
+	//http://stackoverflow.com/a/4073451/4710047
+
+	taskId := r.PostFormValue("id")
+	newName := r.PostFormValue("name")
+	
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+	stmt, err := tx.Prepare("UPDATE Task SET  name=? WHERE id=?", newName, taskId)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	resp,err := stmt.Exec(taskName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tx.Commit()
+
+	//updatedTask := models.Task{Id: int(new_task_id), Name: taskName}
+
+
+    if err := json.NewEncoder(w).Encode(newTask); err != nil {
+        panic(err)
+    }
+}
+
 func ViewTask(w http.ResponseWriter, r *http.Request){
 	//note: r.FormValue searches for key in GET queries, then POST data fields, then PUT data fields
     taskId,_ := strconv.Atoi(r.FormValue("id"))
@@ -199,7 +233,7 @@ func main(){
     //todo: can name each route in order to reverse them. 
     s := r.PathPrefix("/task").Subrouter()
     s.HandleFunc("/", CreateTask).Methods("PUT")
-    // s.HandleFunc("/{id:[0-9]+}/", UpdateTask).Methods("POST")
+    s.HandleFunc("/{id:[0-9]+}/", UpdateTask).Methods("POST")
     s.HandleFunc("/", ViewAllTasks).Methods("GET")
     s.HandleFunc("/{id:[0-9]+}/", ViewTask).Methods("GET")
     s.HandleFunc("/{id:[0-9]+}/", DeleteTask).Methods("DELETE")
