03bac777cf4e87026d0f4bd8d1643fd2541bec7a
himanshuo <ho2es@virginia.edu>
2015-11-26 11:30:24 -0500

    format

--- main.go
+++ main.go
@@ -1,32 +1,31 @@
 package main
-import ( 
-	"github.com/himanshuo/gameoflife/models"
-	"net/http"
-	"github.com/gorilla/mux"
-	"log"
+
+import (
 	"encoding/json"
+	"github.com/gorilla/mux"
+	"github.com/himanshuo/gameoflife/models"
 	"html/template"
+	"log"
+	"net/http"
 	"strconv"
-	//_ is in order to import a package solely for its side-effects at initialization. 
+	//_ is in order to import a package solely for its side-effects at initialization.
 	//In this case, go-sqlite3's side effects are allowing sqlite3 to be usable as a
 	//database for  sql.Open
-	_ "github.com/mattn/go-sqlite3"  
 	"database/sql"
 	"fmt"
-
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var db *sql.DB
 
-func init(){
-	
+func init() {
 
 	var err error
 	db, err = sql.Open("sqlite3", "./data/data.db")
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	sqlStmt := `
 		create table Task (id integer not null primary key autoincrement, name text);
 	`
@@ -35,20 +34,16 @@ func init(){
 		log.Printf("%q", err)
 		return
 	}
-	
-	
 
-	
 }
 
 //views
-func Home(w http.ResponseWriter, r *http.Request){
+func Home(w http.ResponseWriter, r *http.Request) {
 	rows, err := db.Query("select id, name from Task")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer rows.Close()
-
 
 	Tasks := []models.Task{}
 
@@ -56,28 +51,24 @@ func Home(w http.ResponseWriter, r *http.Request){
 		var id int
 		var name string
 		rows.Scan(&id, &name)
-		cur_task := models.Task{Id:id, Name:name}
+		cur_task := models.Task{Id: id, Name: name}
 		Tasks = append(Tasks, cur_task)
 	}
-	
 
 	t, _ := template.ParseFiles("views/static/templates/base.html")
-    t.Execute(w, Tasks)
-
-	
+	t.Execute(w, Tasks)
 
 }
 
-
 //API
-func CreateTask(w http.ResponseWriter, r *http.Request){
+func CreateTask(w http.ResponseWriter, r *http.Request) {
 	//note: r.FormValue searches for key in POST data fields, then PUT data fields
 	//2 types of POST submissions: application/x-www-form-urlencoded AND multipart/form-data.
 	// need to understand both. Generally speaking, urlencoded takes up extra space so is for normal post requests. multipart form-data does not increase space usage by a lot so is for uploading files
 	//http://stackoverflow.com/a/4073451/4710047
-	
+
 	taskName := r.PostFormValue("name")
-	
+
 	tx, err := db.Begin()
 	if err != nil {
 		log.Fatal(err)
@@ -87,7 +78,7 @@ func CreateTask(w http.ResponseWriter, r *http.Request){
 		log.Fatal(err)
 	}
 	defer stmt.Close()
-	resp,err := stmt.Exec(taskName)
+	resp, err := stmt.Exec(taskName)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -95,34 +86,32 @@ func CreateTask(w http.ResponseWriter, r *http.Request){
 	tx.Commit()
 
 	new_task_id, err := resp.LastInsertId()
-	if err!=nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 
 	newTask := models.Task{Id: int(new_task_id), Name: taskName}
 
-
-    if err := json.NewEncoder(w).Encode(newTask); err != nil {
-        panic(err)
-    }
+	if err := json.NewEncoder(w).Encode(newTask); err != nil {
+		panic(err)
+	}
 }
 
-
-func UpdateTask(w http.ResponseWriter, r *http.Request){
+func UpdateTask(w http.ResponseWriter, r *http.Request) {
 	//note: r.FormValue searches for key in POST data fields, then PUT data fields
 	//2 types of POST submissions: application/x-www-form-urlencoded AND multipart/form-data.
 	// need to understand both. Generally speaking, urlencoded takes up extra space so is for normal post requests. multipart form-data does not increase space usage by a lot so is for uploading files
 	//http://stackoverflow.com/a/4073451/4710047
 	log.Printf("called update task")
-	 vars := mux.Vars(r)
-   
+	vars := mux.Vars(r)
+
 	taskId, err := strconv.Atoi(vars["id"])
-	
-	if err != nil{
+
+	if err != nil {
 		log.Fatal(err)
 	}
 	newName := r.PostFormValue("name")
-	
+
 	tx, err := db.Begin()
 	if err != nil {
 		log.Fatal(err)
@@ -132,7 +121,7 @@ func UpdateTask(w http.ResponseWriter, r *http.Request){
 		log.Fatal(err)
 	}
 	defer stmt.Close()
-	_,err = stmt.Exec(newName, taskId)
+	_, err = stmt.Exec(newName, taskId)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -141,28 +130,27 @@ func UpdateTask(w http.ResponseWriter, r *http.Request){
 
 	updatedTask := models.Task{Id: int(taskId), Name: newName}
 
-
-    if err := json.NewEncoder(w).Encode(updatedTask); err != nil {
-         panic(err)
-     }
+	if err := json.NewEncoder(w).Encode(updatedTask); err != nil {
+		panic(err)
+	}
 }
 
-func ViewTask(w http.ResponseWriter, r *http.Request){
+func ViewTask(w http.ResponseWriter, r *http.Request) {
 	//note: r.FormValue searches for key in GET queries, then POST data fields, then PUT data fields
-    log.Printf("view task called")
-    vars := mux.Vars(r)
-   
-    taskId,err := strconv.Atoi(vars["id"])
-    log.Printf(vars["id"])
-    log.Printf("%s",taskId)
-    
-    if err != nil{
-    	log.Fatal(err)
-    }
+	log.Printf("view task called")
+	vars := mux.Vars(r)
 
-    q := fmt.Sprintf("select id, name from Task where id=%d", taskId)
+	taskId, err := strconv.Atoi(vars["id"])
+	log.Printf(vars["id"])
+	log.Printf("%s", taskId)
 
-    rows, err := db.Query(q)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	q := fmt.Sprintf("select id, name from Task where id=%d", taskId)
+
+	rows, err := db.Query(q)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -172,20 +160,19 @@ func ViewTask(w http.ResponseWriter, r *http.Request){
 	var id int
 	var name string
 	rows.Scan(&id, &name)
-	cur_task := models.Task{Id:id, Name:name}
-    	log.Printf("%v", cur_task)
-    if err := json.NewEncoder(w).Encode(cur_task); err != nil {
-        panic(err)
-    }
+	cur_task := models.Task{Id: id, Name: name}
+	log.Printf("%v", cur_task)
+	if err := json.NewEncoder(w).Encode(cur_task); err != nil {
+		panic(err)
+	}
 }
 
-func ViewAllTasks(w http.ResponseWriter, r *http.Request){
+func ViewAllTasks(w http.ResponseWriter, r *http.Request) {
 	rows, err := db.Query("select id, name from Task")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer rows.Close()
-
 
 	Tasks := []models.Task{}
 
@@ -193,19 +180,18 @@ func ViewAllTasks(w http.ResponseWriter, r *http.Request){
 		var id int
 		var name string
 		rows.Scan(&id, &name)
-		cur_task := models.Task{Id:id, Name:name}
+		cur_task := models.Task{Id: id, Name: name}
 		Tasks = append(Tasks, cur_task)
 	}
-	
 
-    if err := json.NewEncoder(w).Encode(Tasks); err != nil {
-        panic(err)
-    }
+	if err := json.NewEncoder(w).Encode(Tasks); err != nil {
+		panic(err)
+	}
 }
 
-func DeleteTask(w http.ResponseWriter, r *http.Request){
-	taskId:= r.PostFormValue("id")
-	
+func DeleteTask(w http.ResponseWriter, r *http.Request) {
+	taskId := r.PostFormValue("id")
+
 	tx, err := db.Begin()
 	if err != nil {
 		log.Fatal(err)
@@ -215,50 +201,40 @@ func DeleteTask(w http.ResponseWriter, r *http.Request){
 		log.Fatal(err)
 	}
 	defer stmt.Close()
-	_,err = stmt.Exec(taskId)
+	_, err = stmt.Exec(taskId)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	tx.Commit()
-	
-	
+
 }
 
-
-
-
-
-func main(){	
+func main() {
 
 	r := mux.NewRouter()
 
-
-
 	//views
-    r.HandleFunc("/", Home)
-    
+	r.HandleFunc("/", Home)
 
-    //API
+	//API
 
-    //task
+	//task
 
-    //todo: can name each route in order to reverse them. 
-    s := r.PathPrefix("/task").Subrouter()
-    s.HandleFunc("/", CreateTask).Methods("PUT")
-    s.HandleFunc("/{id:[0-9]+}/", UpdateTask).Methods("POST")
-    s.HandleFunc("/", ViewAllTasks).Methods("GET")
-    s.HandleFunc("/{id:[0-9]+}/", ViewTask).Methods("GET")
-    s.HandleFunc("/{id:[0-9]+}/", DeleteTask).Methods("DELETE")
-    
+	//todo: can name each route in order to reverse them.
+	s := r.PathPrefix("/task").Subrouter()
+	s.HandleFunc("/", CreateTask).Methods("PUT")
+	s.HandleFunc("/{id:[0-9]+}/", UpdateTask).Methods("POST")
+	s.HandleFunc("/", ViewAllTasks).Methods("GET")
+	s.HandleFunc("/{id:[0-9]+}/", ViewTask).Methods("GET")
+	s.HandleFunc("/{id:[0-9]+}/", DeleteTask).Methods("DELETE")
 
-   
-    //static files
-    fs := http.FileServer(http.Dir("views/static"))
+	//static files
+	fs := http.FileServer(http.Dir("views/static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-    http.Handle("/", r)
-    
+	http.Handle("/", r)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
 	db.Close()
--- tests/API_test.go
+++ tests/API_test.go
@@ -1,26 +1,22 @@
 package tests
 
 import (
-	"testing"
-	"net/http"
-	"strings"
-	"github.com/himanshuo/gameoflife/models"
 	"encoding/json"
 	"fmt"
-	
+	"github.com/himanshuo/gameoflife/models"
+	"net/http"
+	"strings"
+	"testing"
 )
 
-
-
-func TestGetTasks(t *testing.T){
-	_,err := http.Get("http://localhost:8080/")
-	if err != nil{
+func TestGetTasks(t *testing.T) {
+	_, err := http.Get("http://localhost:8080/")
+	if err != nil {
 		t.Errorf("TestGetTasks Failed with err %s", err)
 	}
 }
 
-
-func TestCRUDTasks(t *testing.T){
+func TestCRUDTasks(t *testing.T) {
 	//todo: a url_for(NAME) module would be helpful. I think the gorilla thing might already have it.
 	name := "Test Task 1"
 	var id int
@@ -28,21 +24,20 @@ func TestCRUDTasks(t *testing.T){
 
 	//create task
 	body := strings.NewReader(fmt.Sprintf("name=%s", name))
-	req, err:= http.NewRequest("PUT","http://localhost:8080/task/", 
+	req, err := http.NewRequest("PUT", "http://localhost:8080/task/",
 		body)
-	if err != nil{
+	if err != nil {
 		t.Errorf("request not built properly")
 	}
-	req.Header.Add("Content-Type","application/x-www-form-urlencoded")
-	resp, err:= http.DefaultClient.Do(req)
-	if err != nil{
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
 		t.Errorf("could not create task: %$", err)
 	}
 
-	
 	err = json.NewDecoder(resp.Body).Decode(&task)
-	if err != nil{
-		t.Errorf("TestCRUDTasks got invalid response %s",  err)
+	if err != nil {
+		t.Errorf("TestCRUDTasks got invalid response %s", err)
 	}
 	if task.Name != name {
 		t.Errorf("TestCRUDTasks returned invalid name: %s, %s", task.Name, resp.Body)
@@ -50,43 +45,43 @@ func TestCRUDTasks(t *testing.T){
 	id = task.Id
 
 	//read task
-	resp,err = http.Get(fmt.Sprintf("http://localhost:8080/task/%d/", id))
-	if err != nil{
+	resp, err = http.Get(fmt.Sprintf("http://localhost:8080/task/%d/", id))
+	if err != nil {
 		t.Errorf("TestCRUDTasks got invalid read response %s: %s", err, resp)
 	}
 	//nullify task
 	task = models.Task{}
 
 	err = json.NewDecoder(resp.Body).Decode(&task)
-	if err != nil{
-		t.Errorf("TestCRUDTasks got invalid response %s",  err)
+	if err != nil {
+		t.Errorf("TestCRUDTasks got invalid response %s", err)
 	}
-	if task.Id != id{
+	if task.Id != id {
 		t.Errorf("TestCRUDTasks read got invalid id %s", task.Id)
-	} 
-	if task.Name != name{
+	}
+	if task.Name != name {
 		t.Errorf("TestCRUDTasks read got invalid name %s", task.Name)
-	} 
+	}
 
 	//update task
 	name = "New Test Task Name 1"
 	updateUrl := fmt.Sprintf("http://localhost:8080/task/%d/", id)
 	body = strings.NewReader(fmt.Sprintf("name=%s", name))
-    	req, err = http.NewRequest("POST", updateUrl, body)
-    	req.Header.Add("Content-Type","application/x-www-form-urlencoded")
-    	if err != nil{
-    		t.Errorf("TestCRUDTasks invalid update request %s", err)
-    	}
-    	
-    	resp, err = http.DefaultClient.Do(req)
-    	if err != nil{
-    		t.Errorf("TestCRUDTasks invalid update response %s", err)
-    	}
-    	//nullify task
+	req, err = http.NewRequest("POST", updateUrl, body)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	if err != nil {
+		t.Errorf("TestCRUDTasks invalid update request %s", err)
+	}
+
+	resp, err = http.DefaultClient.Do(req)
+	if err != nil {
+		t.Errorf("TestCRUDTasks invalid update response %s", err)
+	}
+	//nullify task
 	task = models.Task{}
 
 	err = json.NewDecoder(resp.Body).Decode(&task)
-	if err != nil{
+	if err != nil {
 		t.Errorf("TestCRUDTasks could not decode updated task %s", err)
 	}
 	if task.Id != id {
@@ -96,41 +91,37 @@ func TestCRUDTasks(t *testing.T){
 		t.Errorf("TestCRUDTasks update task did not have correct name %d", task.Name)
 	}
 
-
 	//read task
-	resp,err = http.Get(fmt.Sprintf("http://localhost:8080/task/%d/", id))
-	if err != nil{
+	resp, err = http.Get(fmt.Sprintf("http://localhost:8080/task/%d/", id))
+	if err != nil {
 		t.Errorf("TestCRUDTasks got invalid read response %s: %s", err, resp)
 	}
 	//nullify task
 	task = models.Task{}
 
 	err = json.NewDecoder(resp.Body).Decode(&task)
-	if err != nil{
-		t.Errorf("TestCRUDTasks got invalid response %s",  err)
+	if err != nil {
+		t.Errorf("TestCRUDTasks got invalid response %s", err)
 	}
-	if task.Id != id{
+	if task.Id != id {
 		t.Errorf("TestCRUDTasks read got invalid id %s", task.Id)
-	} 
-	if task.Name != name{
+	}
+	if task.Name != name {
 		t.Errorf("TestCRUDTasks read got invalid name %s", task.Name)
-	} 
-
+	}
 
 	//delete task
 	deleteUrl := fmt.Sprintf("http://localhost:8080/task/%d/", id)
 	req, err = http.NewRequest("DELETE", deleteUrl, nil)
-    	if err != nil{
-    		t.Errorf("TestCRUDTasks invalid delete request %s", err)
-    	}
-    	
-    	resp, err = http.DefaultClient.Do(req)
-    	if err != nil{
-    		t.Errorf("TestCRUDTasks invalid delete response %s", err)
-    	}
-    	
-	
-	
+	if err != nil {
+		t.Errorf("TestCRUDTasks invalid delete request %s", err)
+	}
+
+	resp, err = http.DefaultClient.Do(req)
+	if err != nil {
+		t.Errorf("TestCRUDTasks invalid delete response %s", err)
+	}
+
 }
 
 // func TestCreateTasksWithSameName(t *testing.T){
@@ -154,11 +145,9 @@ func TestCRUDTasks(t *testing.T){
 // 	}
 // }
 
-
 // func TestDeleteNonexistantTask(t *testing.T){
 // 	resp,err := http.Get("http://localhost:8080/")
 // 	if err != nil{
 // 		t.FailNow()
 // 	}
 // }
-
