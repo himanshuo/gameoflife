be66f21fe9fb55051585198a312c1e1cab0d9f69
himanshuo <ho2es@virginia.edu>
2015-11-29 13:17:42 -0500

    checkError function to DRY

--- tests/API_test.go
+++ tests/API_test.go
@@ -1,8 +1,8 @@
 package tests
 import (
-	//"encoding/json"
+	"encoding/json"
 	"fmt"
-	//"github.com/himanshuo/gameoflife/models"
+	"github.com/himanshuo/gameoflife/models"
 	"net/http"
 	"strings"
 	"testing"
@@ -10,13 +10,13 @@ import (
 )
 
 //helper function in order to check errors
-func checkError(t *testing.T, err){
+func checkError(t *testing.T, err error){
     	if err != nil {
     		//get caller function
 		pc := make([]uintptr, 10)  // at least 1 entry needed
 	    	runtime.Callers(2, pc)
 		f := runtime.FuncForPC(pc[0])
-	    	file, line := f.FileLine(pc[0])
+	    	_, line := f.FileLine(pc[0])
 	    	funcName := strings.Split(f.Name(),".")[1]
 		t.Errorf("[%s][line %d]:%s:%s", funcName, line, err)    		
     	}
@@ -28,93 +28,79 @@ func TestHomeView(t *testing.T) {
 	checkError(t, err)
 }
 
-// //helper to view a given task
-// func readTask(t *testing.T, task models.Task) {
-// 	resp, err := http.Get(fmt.Sprintf("http://localhost:8080/task/%d/", task.Id))
-// 	checkError(t, "Invalid Read Response:%s", err)
-// 	readTask := models.Task{}
-// 	if err = json.NewDecoder(resp.Body).Decode(&readTask); err != nil {
-// 		t.Errorf("TestCRUDTasks got invalid response %s", err)
-// 	}
-// 	if readTask.Id != task.Id {
-// 		t.Errorf("TestCRUDTasks read got invalid id %s", readTask.Id)
-// 	}
-// 	if readTask.Name != task.Name {
-// 		t.Errorf("TestCRUDTasks read got invalid name %s", readTask.Name)
-// 	}
-// }
+//helper to view a given task
+func readTask(t *testing.T, task models.Task) {
+	resp, err := http.Get(fmt.Sprintf("http://localhost:8080/task/%d/", task.Id))
+	checkError(t, err)
+	readTask := models.Task{}
+	err = json.NewDecoder(resp.Body).Decode(&readTask)
+	checkError(t, err)
+	if readTask.Id != task.Id {
+		t.Errorf("TestCRUDTasks read got invalid id %s", readTask.Id)
+	}
+	if readTask.Name != task.Name {
+		t.Errorf("TestCRUDTasks read got invalid name %s", readTask.Name)
+	}
+}
 
-// //helper to create a task given a name
-// func createTask(t *testing.T, name string) models.Task {
-// 	task := models.Task{}
-// 	body := strings.NewReader(fmt.Sprintf("name=%s", name))
-// 	req, err := http.NewRequest("PUT", "http://localhost:8080/task/",
-// 		body)
-// 	if err != nil {
-// 		t.Errorf("request not built properly")
-// 	}
-// 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-// 	resp, err := http.DefaultClient.Do(req)
-// 	if err != nil {
-// 		t.Errorf("could not create task: %s", err)
-// 	}
-// 	if err = json.NewDecoder(resp.Body).Decode(&task); err != nil {
-// 		t.Errorf("TestCRUDTasks got invalid response %s", err)
-// 	}
-// 	if task.Name != name {
-// 		t.Errorf("TestCRUDTasks returned invalid name: %s, %s", task.Name, resp.Body)
-// 	}
-// 	return task
+//helper to create a task given a name
+func createTask(t *testing.T, name string) models.Task {
+	task := models.Task{}
+	body := strings.NewReader(fmt.Sprintf("name=%s", name))
+	req, err := http.NewRequest("PUT", "http://localhost:8080/task/",
+		body)
+	checkError(t, err)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	resp, err := http.DefaultClient.Do(req)
+	checkError(t, err)
+	err = json.NewDecoder(resp.Body).Decode(&task)
+	checkError(t, err)
+	if task.Name != name {
+		t.Errorf("TestCRUDTasks returned invalid name: %s, %s", task.Name, resp.Body)
+	}
+	return task
 
-// }
+}
 
-// //helper to update a task given its id and a new name
-// func updateTask(t *testing.T, task models.Task) models.Task {
-// 	updateUrl := fmt.Sprintf("http://localhost:8080/task/%d/", task.Id)
-// 	body := strings.NewReader(fmt.Sprintf("name=%s", task.Name))
-// 	req, err := http.NewRequest("POST", updateUrl, body)
-// 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-// 	if err != nil {
-// 		t.Errorf("TestCRUDTasks invalid update request %s", err)
-// 	}
-// 	resp, err := http.DefaultClient.Do(req)
-// 	if err != nil {
-// 		t.Errorf("TestCRUDTasks invalid update response %s", err)
-// 	}
-// 	updatedTask := models.Task{}
-// 	if err = json.NewDecoder(resp.Body).Decode(&updatedTask); err != nil {
-// 		t.Errorf("TestCRUDTasks could not decode updated task %s", err)
-// 	}
-// 	if updatedTask.Id != task.Id {
-// 		t.Errorf("TestCRUDTasks update task did not have correct id %d", updatedTask.Id)
-// 	}
-// 	if updatedTask.Name != task.Name {
-// 		t.Errorf("TestCRUDTasks update task did not have correct name %d", updatedTask.Name)
-// 	}
-// 	return updatedTask
-// }
+//helper to update a task given its id and a new name
+func updateTask(t *testing.T, task models.Task) models.Task {
+	updateUrl := fmt.Sprintf("http://localhost:8080/task/%d/", task.Id)
+	body := strings.NewReader(fmt.Sprintf("name=%s", task.Name))
+	req, err := http.NewRequest("POST", updateUrl, body)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	checkError(t, err)
+	resp, err := http.DefaultClient.Do(req)
+	checkError(t, err)
+	updatedTask := models.Task{}
+	err = json.NewDecoder(resp.Body).Decode(&updatedTask)
+	checkError(t, err)
+	if updatedTask.Id != task.Id {
+		t.Errorf("TestCRUDTasks update task did not have correct id %d", updatedTask.Id)
+	}
+	if updatedTask.Name != task.Name {
+		t.Errorf("TestCRUDTasks update task did not have correct name %d", updatedTask.Name)
+	}
+	return updatedTask
+}
 
-// //helper to delete a task
-// func deleteTask(t *testing.T, task models.Task) {
-// 	deleteUrl := fmt.Sprintf("http://localhost:8080/task/%d/", task.Id)
-// 	req, err := http.NewRequest("DELETE", deleteUrl, nil)
-// 	if err != nil {
-// 		t.Errorf("TestCRUDTasks invalid delete request %s", err)
-// 	}
-// 	if _, err = http.DefaultClient.Do(req); err != nil {
-// 		t.Errorf("TestCRUDTasks invalid delete response %s", err)
-// 	}
-// }
+//helper to delete a task
+func deleteTask(t *testing.T, task models.Task) {
+	deleteUrl := fmt.Sprintf("http://localhost:8080/task/%d/", task.Id)
+	req, err := http.NewRequest("DELETE", deleteUrl, nil)
+	checkError(t, err)
+	_, err = http.DefaultClient.Do(req)
+	checkError(t, err)
+}
 
-// //test all crud options
-// func TestCRUDTasks(t *testing.T) {
-// 	task := createTask(t, "Test Task 1")
-// 	readTask(t, task)
-// 	task.Name = "Test Task 2"
-// 	task = updateTask(t, task)
-// 	readTask(t, task)
-// 	deleteTask(t, task)
-//}
+//test all crud options
+func TestCRUDTasks(t *testing.T) {
+	task := createTask(t, "Test Task 1")
+	readTask(t, task)
+	task.Name = "Test Task 2"
+	task = updateTask(t, task)
+	readTask(t, task)
+	deleteTask(t, task)
+}
 
 // func TestCreateTasksWithSameName(t *testing.T){
 // 	resp,err := http.Get("http://localhost:8080/")
