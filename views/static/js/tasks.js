8ef0845967d4ab0dce27436bb3440934e220c5cc
himanshuo <ho2es@virginia.edu>
2015-12-03 09:37:06 -0500

    Revert "added node modules"
    This reverts commit 39b35062787d44b8d279f8bc157a72284a36fcb7.

--- views/static/js/tasks.js
+++ views/static/js/tasks.js
@@ -5,40 +5,31 @@
 //			taskdescription
 //			other task stuff
 
-var React = require('react');
-var ReactDOM = require('react-dom');
-
 var TaskList = React.createClass({
 	displayName: 'TaskList',
 	render: function(){
 		return (
-			<div className="tasklist">
-				<TaskBox />
-				<TaskBox />
-				<TaskBox />
-				<TaskBox />
-			</div>	
-		);
+			React.createElement(
+				'div', 
+				{className: "tasklist"},
+				"!!HOW DO WE USE SOME OTHER VARIABLE????!!"
+			)
+		)
 	}
 });
-
-
 
 var TaskBox = React.createClass({displayName: 'TaskBox',
   render: function(){
     return (
-      	<div className="taskbox">
-      		Contents of TaskBox
-      	</div>      
+      React.createElement('div', {className: "commentBox"},
+        "Hello, world! I am a CommentBox."
+      )
     );
   }
 });
 
 
-
-
-
 ReactDOM.render(
-<TaskList />,
+  React.createElement(TaskBox, null),
   document.getElementById('content')
 );
