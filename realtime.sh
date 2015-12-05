bd8a80fc0c8c5f28619c463dd363ddd285fe7193
himanshuo <ho2es@virginia.edu>
2015-12-04 23:18:00 -0500

    basic
    inotifywait. running main.go finding PID to kill.

--- realtime.sh
+++ realtime.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 clear
-echo "Good morning, world."
+# determine which directory to watch
+DIRNAME="."
+MYPID=""
+echo "Starting Server"
+go run main.go &
+MYPID=$!
+kill $MYPID
+go run main.go &
+MYPID=$!
+
+#print variable on a screen
+#inotifywait -m -r --event modify $DIRNAME | while read line; do
+#	echo "Restarting Server because $line"
+#	kill $MYPID
+#	go run main.go &
+#	MYPID=$!
+#done
