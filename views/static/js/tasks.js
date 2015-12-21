7b22f8b99a4d49ee2cfca861add130d760bfd9df
himanshuo <ho2es@virginia.edu>
2015-12-21 14:02:36 -0500

    setup for browserify

--- views/static/js/tasks.js
+++ views/static/js/tasks.js
@@ -8,6 +8,11 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var data = [
+  {id: 1, name: "Pete Hunt", desc: "This is one comment"},
+  {id: 2, name: "Jordan Walke", desc: "This is *another* comment"}
+];
+
 var TaskList = React.createClass({
 	displayName: 'TaskList',
 	render: function(){
@@ -28,7 +33,7 @@ var TaskBox = React.createClass({displayName: 'TaskBox',
   render: function(){
     return (
       	<div className="taskbox">
-      		Contents of TaskBox
+      		This is a taskbox.
       	</div>      
     );
   }
