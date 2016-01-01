436563a82f41603d68f3421588a7122966a68fc9
himanshuo <ho2es@virginia.edu>
2016-01-01 17:33:44 -0500

    time issues

--- models/task.go
+++ models/task.go
@@ -11,17 +11,17 @@ type Task struct {
 	Description string `json:"description"`
 	FailureCriteria string `json:"failureCriteria"`
 	AcceptanceCriteria string `json:"acceptanceCriteria"`
-	Deadline JSONTime `json:"deadline"` 
-	FailTime JSONTime `json:"failTime"` 
-	AcceptTime JSONTime `json:"acceptTime"` 
+	Deadline time.Time `json:"deadline"` 
+	FailTime time.Time `json:"failTime"` 
+	AcceptTime time.Time `json:"acceptTime"` 
 	AchievementPoints int `json:"achievementPoints"`
 	SubTasks []Task `json:"subtasks"`
 	Goals []Goal `json:"goals"`
 	Progress Progress `json:"progress"`
 	//todo: handle recurring in a smart way
 	Recurring bool `json:"recurring"`
-	RecurStart JSONTime `json:"recurStart"`
-	RecurEnd JSONTime `json:"recurEnd"`
+	RecurStart time.Time `json:"recurStart"`
+	RecurEnd time.Time `json:"recurEnd"`
 	//time.Duration is just int64
 	//todo: unclear whether json conversion handles Duration->int64->string
 	//todo: if not, will have to make marshal and unmarshal functions as for JSONtime
--- tests/task_model_test.go
+++ tests/task_model_test.go
@@ -2,120 +2,75 @@ package tests
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/himanshuo/gameoflife/models"
-	"net/http"
-	"runtime"
 	"strings"
 	"testing"
+	"time"
+	"fmt"
+	"reflect"
 )
 
 
-//helper function in order to check errors
-func checkError(t *testing.T, err error) {
-	if err != nil {
-		//get caller function
-		pc := make([]uintptr, 10) // at least 1 entry needed
-		runtime.Callers(2, pc)
-		f := runtime.FuncForPC(pc[0])
-		_, line := f.FileLine(pc[0])
-		funcName := strings.Split(f.Name(), ".")[1]
-		t.Errorf("[%s][line %d]:%s:%s", funcName, line, err)
-	}
-}
 
 //things to test:
-//basic json <-> Task
-//JSONTime json <-> task
-//Duration task <-> json
 //subtask <->
 //empty values for each each 
 
-//HOW TO: json to map
-//var dat map[string]interface{}
-//    if err := json.Unmarshal(byt, &dat); err != nil {
- //       panic(err)
-  //  }
-  //  fmt.Println(dat)
-//    num := dat["num"].(float64)
-  //  fmt.Println(num)
-
-var testData = []struct {
-	taskObj Task
+type TestCase struct {
+	taskObj models.Task
 	taskJSON string // json
-}{
-	{ 
-		Task{1, "task1", "test task", "fail criteria no exist", "accept criteria no exist", "0","0","0",0, []Task{}, []Goal{}, Progress.OPEN, false, "0", "0", 0},
-		`{id: 1, name: "task1", description:"test task", failureCriteria:"fail criteria no exist", acceptanceCriteria:"accept criteria no exist", deadline:"0",failTime:"0",acceptTime:"0",achievementPoints:0, subtasks: [], goals:[], progress:0, recurring:false, recurStart: "0", recurEnd:"0", recurInterval:0}`
-	}
-	// {"a_name","a_author","", false, true},            // simple
-	// {"a_name","a_author","0", false, true},            // 0 version
-	// {"a_name","a_author","1", false, true},            // version
-	// {"a_name","a_author","0.9.1", false, true},        // complex version
-	
-	// {"s", "s", "0", false, true},					   // single char
-	// {" 1234567890/.,;'[]\"@#$%^&*()_+}{|:\"?><", "s", "0", false, true},	// weird char
-		
-	// {"a_name","a_author","0", true, true},            // simple multipath
-	// {"a_name","a_author","1", true, true},            // version multipath
-	// {"a_name","a_author","0.9.1", true, true},        // complex version multipath
-	// {"s", "s", "0", false, true},					   // single char multipath
-	// {"1234567890/.,;'[]\" ~!@#$%^&*()_+}{|:\"?><", "s", "0", false, true},	// weird char multipath
-	
-	// //bad
-	// {".", ".", "0", false, false},					   // dot input
-	// {"..", "..", "0", false, false},				   // double dot input
-	// {"//", "//", "0", false, false},				   // double slash
-	// {"a_name","a_author","", false, true},            // simple
-	// {"a_name","a_author","0", false, true},            // simple 0 version
-	// {"a_name","a_author","1", false, true},            // version
-	// {"a_name","a_author","0.9.1", false, true},        // complex version
-	
-	// {"s", "s", "0", false, true},					   // single char
-	// {" 1234567890/.,;'[]\"@#$%^&*()_+}{|:\"?><", "s", "0", false, true},	// weird char
-		
-	// {"a_name","a_author","0", true, true},            // simple multipath
-	// {"a_name","a_author","1", true, true},            // version multipath
-	// {"a_name","a_author","0.9.1", true, true},        // complex version multipath
-	// {"s", "s", "0", false, true},					   // single char multipath
-	// {"1234567890/.,;'[]\" ~!@#$%^&*()_+}{|:\"?><", "s", "0", false, true},	// weird char multipath
-	
-	// //bad multipath
-	// {".", ".", "0", true, false},					   // dot input
-	// {"..", "..", "0", true, false},				   // double dot input
-	// {"//", "//", "0", true, false},				   // double slash
-	
-	// //good single path
-	// {"a_name","a_author","0", false, true},            // simple
-	// {"a_name","a_author","1", false, true},            // version
-	// {"a_name","a_author","0.9.1", false, true},        // complex version
-	
-	// {"s", "s", "0", false, true},					   // single char
-	// {" 1234567890/.,;'[]\"@#$%^&*()_+}{|:\"?><", "s", "0", false, true},	// weird char
-		
-	// {"a_name","a_author","0", true, true},            // simple multipath
-	// {"a_name","a_author","1", true, true},            // version multipath
-	// {"a_name","a_author","0.9.1", true, true},        // complex version multipath
-	// {"s", "s", "0", false, true},					   // single char multipath
-	// {"1234567890/.,;'[]\" ~!@#$%^&*()_+}{|:\"?><", "s", "0", false, true},	// weird char multipath
-	
-	// //bad single path
-	// {".", ".", "0", false, false},					   // dot input
-	// {"..", "..", "0", false, false},				   // double dot input
-	// {"//", "//", "0", false, false},				   // double slash
-	
 }
+
+var testData []TestCase
+var curTime time.Time
+var curTimeJSON []byte
+var emptyTaskJSON []byte
+
+func init(){
+	curTime = time.Now()
+	curTimeJSON, _ = json.Marshal(curTime)
+	emptyTaskJSON, _ = json.Marshal(models.Task{})
+
+	testData = []TestCase{
+		//basic
+		{ 
+			models.Task{1, "task1", "test", "failT", "acceptT", curTime,curTime,curTime,0, []models.Task{}, []models.Goal{}, models.OPEN, false, curTime, curTime, 0},
+			fmt.Sprintf(`{"id":1,"name":"task1","description":"test","failureCriteria":"failT","acceptanceCriteria":"acceptT","deadline":%[1]s,"failTime":%[1]s,"acceptTime":%[1]s,"achievementPoints":0,"subtasks":[],"goals":[],"progress":0,"recurring":false,"recurStart":%[1]s,"recurEnd":%[1]s,"recurInterval":0}`, curTimeJSON),
+		},
+		//subtasks
+		{ 
+			models.Task{1, "task1", "test", "failT", "acceptT", curTime,curTime,curTime,0, []models.Task{models.Task{},models.Task{}}, []models.Goal{}, models.OPEN, false, curTime, curTime, 0},
+			fmt.Sprintf(`{"id":1,"name":"task1","description":"test","failureCriteria":"failT","acceptanceCriteria":"acceptT","deadline":%[1]s,"failTime":%[1]s,"acceptTime":%[1]s,"achievementPoints":0,"subtasks":[%[2]s,%[2]s],"goals":[],"progress":0,"recurring":false,"recurStart":%[1]s,"recurEnd":%[1]s,"recurInterval":0}`, curTimeJSON, emptyTaskJSON),
+		},
+		
+	}
+}
+
+
 
 //test all crud options
 func TestTaskConversions(t *testing.T) {
-	for i, test := range testData{
+	for i, test := range testData {
 		//json to Task
-		dec := json.NewDecoder(strings.NewReader(jsonStream))
-		var resTask Task
+		dec := json.NewDecoder(strings.NewReader(test.taskJSON))
+		var resTask models.Task
 		if err := dec.Decode(&resTask); err != nil {
-			t.Errorf("TestTaskConversions %d:  EXPECTED: %s->%s ACTUAL: %s->%s ERROR:", i, test.taskObj, test.taskJSON, test.taskObj, resTask, err)
+			t.Errorf("TestTaskConversions %d: %s", i, err)
+		}
+
+
+		if !reflect.DeepEqual(resTask, test.taskObj){
+			t.Errorf("TestTaskConversions %d:  \nEXPECTED: %s \nACTUAL:     %s", i, test.taskObj, resTask)
 		}
 
 		//Task to JSON
-		
+		actualJSON, err := json.Marshal(test.taskObj)
+		if err!= nil{
+			t.Errorf("TestTaskConversions %d: %s", i, err)
+		}
+		if string(actualJSON) != test.taskJSON {
+			t.Errorf("TestTaskConversions %d: \nEXPECTED: %s \nACTUAL:     %s", i, test.taskJSON, actualJSON)
+		}
+
+	}
 }
